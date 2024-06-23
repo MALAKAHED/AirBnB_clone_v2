acc0793de09da0cab1f08fdde6caf2709b9c9211
Amlnasser7 <amlnasser32@gmail.com>
2024-06-24 02:59:27 +0300

    HI

---
+++ tests/setup_mysql_dev.sql
@@ -0,0 +1,22 @@
+-- create database and give the user access to this DATABASE
+-- User: hbnb_dev@localhost
+-- password: hbnb_dev_pwd
+-- hostt: localhost 
+-- Database: hbnb_dev_db
+
+CREATE DATABASE 
+    IF NOT EXISTS hbnb_dev_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+
+GRANT ALL PRIVILEGES 
+ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost'
+IDENTIFIED BY 'hbnb_dev_pwd';
+
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
+
+
+
