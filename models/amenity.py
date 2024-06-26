72f833442b74d895e28061bc968db3e637d973e4
Amlnasser7 <amlnasser32@gmail.com>
2024-06-26 11:21:15 -0700

    10

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,32 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This module defines a Many-Many relationship between amenities and places"""
+import os
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """This class defines an Amenity by various attributes"""
+    _tablename_ = 'amenities'
+    name = Column(
+        String(128), nullable=False
+    ) 
+place_amenity = db.Table('place_amenity',
+     db.Column('place_id', db.Integer, db.ForeignKey('place.id')),
+     db.Column('amenity_id', db.Integer, db.ForeignKey('amenity.id'))
+)
+class Places(db.Model):
+id = db.Column (db.Integer, primary_key=True)
+name = db.Column (db.String(60))
+place_amenities = relationship('Amenity', secondary="place_amenity", viewonly=False)
+def _repr(self):
+return f'<Place: {self.name}>'
+
+
+class Amenity(db.Model):
+id = db.Column (db.Integer, primary_key=True) 
+name = db.Column (db.String(60))
+def repr(self):
+return f'<Amenity: {self.name}>'
