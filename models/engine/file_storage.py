25865b86d4bb4755f7282cc36e3372dc04ebeed5
MALAKAHED <lokamalek776@gmail.com>
2024-06-30 02:13:07 +0300

    solve: delete method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,8 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-        def delete(self, obj=None): 
+
+    def delete(self, obj=None): 
             """delete object"""
             if obj is not None:
                 FileStorage.__objects.pop(
